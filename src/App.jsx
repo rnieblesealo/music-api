3379610c155527462b71309e995e39c25f8bde0b
Rafael Niebles <rnieblesealo@gmail.com>
2025-04-07 17:59:48 -0400

    summary stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { getTopArtists, getSpotifyArtistInfo } from "./scripts/music-search"
 import { useState, useEffect, useRef } from "react"
-import { FaLastfm, FaPlay } from "react-icons/fa6";
-import { FaLastfmSquare } from "react-icons/fa";
 
 const App = () => {
   function capitalize(s) {
@@ -33,8 +31,24 @@ const App = () => {
     )
   }
 
+  const StatCard = ({ data, heading }) => {
+    return (
+      <div className="p-4 rounded-2xl flex flex-col items-center">
+        <p className="text-center my-2">{heading}</p>
+        <img
+          src={data.image}
+          className="w-40 aspect-square object-cover rounded-lg"
+        />
+        <p className="text-center font-bold text-lg my-3">{data.name}</p>
+      </div>
+    )
+  }
+
   const [username, setUsername] = useState("rafaisafar")
   const [artists, setArtists] = useState([])
+  const [mostMainstream, setMostMainstream] = useState(null)
+  const [mostNiche, setMostNiche] = useState(null)
+  const [mainGenre, setMainGenre] = useState(null)
 
   useEffect(() => {
     async function getArtistInfo() {
@@ -61,6 +75,41 @@ const App = () => {
         )
 
         setArtists(artistInfo)
+
+        // set most mainstream (greatest popularity)
+        setMostMainstream(
+          artistInfo.reduce((max, curr) => {
+            return curr.popularity > max.popularity ? curr : max;
+          })
+        )
+
+        // set most niche (least popularity)
+        setMostNiche(
+          artistInfo.reduce((min, curr) => {
+            return curr.popularity < min.popularity ? curr : min;
+          })
+        )
+
+        setMainGenre(() => {
+          // get top match genre for each artist
+          const mainGenres = artistInfo.map((info) => {
+            return info.genres[0] ?? ""
+          }).filter((item) => {
+            return item !== ""
+          })
+
+          // basic dict freq counting
+          const freqCounter = mainGenres.reduce((counter, item) => {
+            counter[item] = (counter[item] || 0) + 1; // add item if unseen; if not just incr. freq
+            return counter;
+          }, {}) // brackets are initialization of dict we use inside func
+
+          const mostFreqItem = Object.keys(freqCounter).reduce((max, item) => {
+            return freqCounter[item] > freqCounter[max] ? item : max;
+          })
+
+          return mostFreqItem;
+        })
       } catch (err) {
         console.error(err)
       }
@@ -98,6 +147,27 @@ const App = () => {
     )
   })
 
+  const mostMainstreamComp = mostMainstream && (
+    <StatCard
+      data={mostMainstream}
+      heading="Most mainstream"
+    />
+  )
+
+  const mostNicheComp = mostNiche && (
+    <StatCard
+      data={mostNiche}
+      heading="Most niche"
+    />
+  )
+
+  const mainGenreComp = mainGenre && (
+    <div className="flex flex-col items-center mb-8">
+      <p>Most listened to genre</p>
+      <p className="text-2xl font-bold">{mainGenre ? capitalize(mainGenre) : "unknown"}</p>
+    </div>
+  )
+
   return (
     <div className="flex flex-col items-center text-white p-12 bg-black">
       <h1 className="text-4xl font-extrabold m-4">
@@ -108,8 +178,15 @@ const App = () => {
         type="text"
         placeholder="rafaisafar"
         ref={(me) => inputRef.current = me}
-        className="text-center text-black focus:outline-none border-0 m-4 mb-8 p-2 bg-white rounded-full placeholder:text-gray-500"
+        className="text-center text-black focus:outline-none border-0 m-4 p-2 bg-white rounded-full placeholder:text-gray-500"
       />
+      <div className="">
+        <div className="flex">
+          {mostMainstreamComp}
+          {mostNicheComp}
+        </div>
+        {mainGenreComp}
+      </div>
       <table className="border-separate border-spacing-2">
         <thead>
           <tr className="text-center">
